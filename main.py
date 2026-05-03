d6644bb51a2705d385d0bccdcbd7982ff4d2635f
Valeria-Lee <leev6893a@gmail.com>
2026-05-02 19:58:45 -0600

    fix: agregar rutas de log en main.py

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ app.include_router(traducir.router)
 app.include_router(test_braille.router)
 app.include_router(users.router)
 app.include_router(documents.router)
+app.include_router(logs.router)
 
 class TextoRequest(BaseModel):
     text: str
